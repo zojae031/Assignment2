e32f1394fb8fa3bc9034ae10a475b0b555c493ea
Zojae031 <edgar3@naver.com>
2019-12-25 22:28:21 +0900

    Product class 정의

--- src/data/dao/Product.java
+++ src/data/dao/Product.java
@@ -1,7 +1,48 @@
 package data.dao;
 
 public class Product {
-    public Product() {
+    private int prcdoe;
+    private String prname;
+    private int price;
+    private String manufacture;
 
+    public Product(int prcdoe, String prname, int price, String manufacture) {
+        this.prcdoe = prcdoe;
+        this.prname = prname;
+        this.price = price;
+        this.manufacture = manufacture;
+    }
+
+    public int getPrcdoe() {
+        return prcdoe;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getManufacture() {
+        return manufacture;
+    }
+
+    public String getPrname() {
+        return prname;
+    }
+
+    public void setManufacture(String manufacture) {
+        this.manufacture = manufacture;
+    }
+
+    public void setPrcdoe(int prcdoe) {
+        this.prcdoe = prcdoe;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setPrname(String prname) {
+        this.prname = prname;
     }
 }
+
