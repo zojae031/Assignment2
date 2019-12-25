655cd092f1272483dc4b36dc1d1cf7501ca90589
HomeZe <55151796+HomeZe@users.noreply.github.com>
2019-12-26 01:42:35 +0900

    중복된 setter getter 제거

--- src/data/dao/Product.java
+++ src/data/dao/Product.java
@@ -16,64 +16,44 @@ public class Product {
         this.manufacture = manufacture;
     }
 
-    public int getPrcdoe() {
-        return prcdoe;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public String getManufacture() {
-        return manufacture;
-    }
-
-    public String getPrname() {
-        return prname;
-    }
-
-    public void setManufacture(String manufacture) {
-        this.manufacture = manufacture;
-    }
-
-    public void setPrcdoe(int prcdoe) {
-        this.prcdoe = prcdoe;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public void setPrname(String prname) {
-        this.prname = prname;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public String getManufacture() {
-        return manufacture;
-    }
-
-    public String getPrname() {
-        return prname;
-    }
-
-    public void setManufacture(String manufacture) {
-        this.manufacture = manufacture;
-    }
-
     public void setPrcode(int prcode) {
         this.prcode = prcode;
     }
 
+    public void setPrname(String prname) {
+        this.prname = prname;
+    }
+
     public void setPrice(int price) {
         this.price = price;
     }
 
-    public void setPrname(String prname) {
-        this.prname = prname;
+    public void setManufacture(String manufacture) {
+        this.manufacture = manufacture;
+    }
+
+    public String getPrname() {
+        return prname;
+    }
+
+    public String getManufacture() {
+        return manufacture;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getPrcode() {
+        return prcode;
+    }
+
+    public int getPrcdoe() {
+        return prcdoe;
+    }
+
+    public void setPrcdoe(int prcdoe) {
+        this.prcdoe = prcdoe;
     }
 
 }
